9dfdf0c7db21da5f28cbc61e8f3f38def9749d94
m_sadegh_sh <m.sadegh.sh@gmail.com>
2021-02-02 21:18:27 +0330

    Create HooksController.cs

---
+++ Billing.Sample/Controllers/HooksController.cs
@@ -0,0 +1,28 @@
+﻿namespace Zebble.Billing.Sample
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route("hooks")]
+    public class HooksController : ControllerBase
+    {
+        readonly IRootQueueProcessor _rootQueueProcessor;
+
+        public HooksController(IRootQueueProcessor rootQueueProcessor)
+        {
+            _rootQueueProcessor = rootQueueProcessor;
+        }
+
+        [HttpGet("process/{platform}")]
+        public async Task<string> Process(SubscriptionPlatform platform)
+        {
+            var processedMessages = await _rootQueueProcessor.Process(platform);
+
+            if (processedMessages == 0)
+                return $"No message found to process. ({platform})";
+
+            return $"{processedMessages} messages are processed. ({platform})";
+        }
+    }
+}
