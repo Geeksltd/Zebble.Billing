b53e0520a1f0fc16af5789c2c6b9dd407030ff70
Mohammad Sadegh Shad <mohammadsadegh.shad@geeks.ltd.uk>
2021-02-06 18:57:00 +0330

    Update

--- Billing.Server.AppStore/Configuration/AppStoreOptions.cs
+++ Billing.Server.AppStore/Configuration/AppStoreOptions.cs
@@ -7,12 +7,14 @@
     public class AppStoreOptions : StoreOptionsBase
     {
         public string SharedSecret { get; set; }
-        public AppStoreEnvironment Environment { get; set; }
+        public string Environment { get; set; }
         public Uri HookInterceptorUri { get; set; }
 
         internal new bool Validate()
         {
             if (SharedSecret.IsEmpty()) throw new ArgumentNullException(nameof(SharedSecret));
+
+            if (Environment.IsEmpty()) throw new ArgumentNullException(nameof(Environment));
 
             if (HookInterceptorUri == null) throw new ArgumentNullException(nameof(HookInterceptorUri));
             if (HookInterceptorUri.IsAbsoluteUri == false) throw new InvalidOperationException($"{nameof(HookInterceptorUri)} should be absolute.");
@@ -23,7 +25,7 @@
         internal void Apply(AppleReceiptVerificationSettings @that)
         {
             @that.VerifyReceiptSharedSecret = SharedSecret;
-            @that.VerificationType = Environment == AppStoreEnvironment.Sandbox ? AppleReceiptVerificationType.Sandbox : AppleReceiptVerificationType.Production;
+            @that.VerificationType = Environment == "Sandbox" ? AppleReceiptVerificationType.Sandbox : AppleReceiptVerificationType.Production;
             @that.AllowedBundleIds = new[] { PackageName };
         }
     }
