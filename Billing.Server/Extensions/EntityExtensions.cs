02aa579ff1a8fe5d1feb1bdc2127d652208b2419
Mohammad Sadegh Shad <mohammadsadegh.shad@geeks.ltd.uk>
2021-02-06 12:46:57 +0330

    Update

---
+++ Billing.Server/Extensions/EntityExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Zebble.Billing
+{
+    static partial class EntityExtensions
+    {
+        public static bool RequiresStoreUpdate(this Subscription @this) => @this.IsExpired() || @this.IsCanceled();
+    }
+}
--- Billing.Shared/Extensions/EntityExtensions.cs
+++ Billing.Shared/Extensions/EntityExtensions.cs
@@ -2,12 +2,10 @@
 {
     using Olive;
 
-    public static class EntityExtensions
+    public static partial class EntityExtensions
     {
         public static bool IsExpired(this Subscription @this) => @this.ExpiryDate?.IsInThePast() ?? false;
 
         public static bool IsCanceled(this Subscription @this) => @this.CancellationDate?.IsInThePast() ?? false;
-
-        public static bool RequiresStoreUpdate(this Subscription @this) => @this.IsExpired() || @this.IsCanceled();
     }
 }
