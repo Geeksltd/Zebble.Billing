302152cfb3505dc8ec0010a976771e4f88d667d3
Mohammad Sadegh Shad <mohammadsadegh.shad@geeks.ltd.uk>
2021-02-23 20:40:59 +0330

    Update

--- Billing.Plugin/Android/GooglePlay/BillingContext.Init.cs
+++ Billing.Plugin/Android/GooglePlay/BillingContext.Init.cs
@@ -1,20 +1,7 @@
 ﻿namespace Zebble.Billing
 {
-    using Android.App;
-    using Android.OS;
-
     partial class BillingContext
     {
-        public static void InitApp(Application app)
-        {
-            Xamarin.Essentials.Platform.Init(app);
-        }
-
-        public static void InitActivity(Activity activity, Bundle bundle)
-        {
-            Xamarin.Essentials.Platform.Init(activity, bundle);
-        }
-
         public static string PaymentAuthority => "GooglePlay";
     }
 }
