42cde0a9243bf0343c244bb069cc680db8ee88ef
Mohammad Sadegh Shad <mohammadsadegh.shad@geeks.ltd.uk>
2021-02-24 20:37:30 +0330

    Update

--- Billing.Plugin/Shared/Commands/PurchaseSubscriptionCommand.cs
+++ Billing.Plugin/Shared/Commands/PurchaseSubscriptionCommand.cs
@@ -25,7 +25,7 @@
         {
             try
             {
-#if CAFEBAZAAR
+#if CAFEBAZAAR && ANDROID
                 var purchase = await Billing.PurchaseAsync(Product.Id, Product.ItemType, BillingContext.Current.User.UserId, new PurchaseVerificator());
 #else
                 var purchase = await Billing.PurchaseAsync(Product.Id, Product.ItemType, new PurchaseVerificator());
