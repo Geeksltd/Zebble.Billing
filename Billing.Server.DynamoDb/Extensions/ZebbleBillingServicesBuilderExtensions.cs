ea1653dd47587769d87d1613b0a0bf4eb1369175
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-04-20 11:37:11 +0430

    Use existing AWS configuration methods

--- Billing.Server.DynamoDb/Extensions/ZebbleBillingServicesBuilderExtensions.cs
+++ Billing.Server.DynamoDb/Extensions/ZebbleBillingServicesBuilderExtensions.cs
@@ -1,20 +1,13 @@
 ﻿namespace Zebble.Billing
 {
-    using Microsoft.Extensions.Configuration;
+    using Amazon.DynamoDBv2;
     using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Options;
-    using Olive;
 
     public static partial class ZebbleBillingServicesBuilderExtensions
     {
         public static ZebbleBillingServicesBuilder AddDynamoDb(this ZebbleBillingServicesBuilder builder, string configKey = "ZebbleBilling:DynamoDb")
         {
-            builder.Services.AddOptions<DynamoDbOptions>()
-                            .Configure<IConfiguration>((opts, config) => config.GetSection(configKey)?.Bind(opts))
-                            .Validate(opts => opts.AccessKey.HasValue(), $"{nameof(DynamoDbOptions.AccessKey)} is empty.")
-                            .Validate(opts => opts.SecretKey.HasValue(), $"{nameof(DynamoDbOptions.SecretKey)} is empty.");
-
-            builder.Services.AddScoped(sp => sp.GetRequiredService<IOptions<DynamoDbOptions>>().Value.CreateClient());
+            builder.Services.AddAWSService<IAmazonDynamoDB>();
 
             builder.Services.AddScoped<SubscriptionDbContext>();
 
