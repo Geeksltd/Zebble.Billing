74824e64d93602e3ed5a7beac3a9b859969be907
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-05-04 14:07:59 +0430

    Fix DynamoDb config

--- Billing.Server.DynamoDb/Extensions/ZebbleBillingServicesBuilderExtensions.cs
+++ Billing.Server.DynamoDb/Extensions/ZebbleBillingServicesBuilderExtensions.cs
@@ -1,13 +1,14 @@
 ﻿namespace Zebble.Billing
 {
     using Amazon.DynamoDBv2;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
 
     public static partial class ZebbleBillingServicesBuilderExtensions
     {
         public static ZebbleBillingServicesBuilder AddDynamoDb(this ZebbleBillingServicesBuilder builder, string configKey = "ZebbleBilling:DynamoDb")
         {
-            builder.Services.AddAWSService<IAmazonDynamoDB>();
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IConfiguration>().GetAWSOptions(configKey).CreateServiceClient<IAmazonDynamoDB>());
 
             builder.Services.AddScoped<SubscriptionDbContext>();
 
