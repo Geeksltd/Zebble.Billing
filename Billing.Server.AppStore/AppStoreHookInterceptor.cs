9d258c6845a3c895136918d672bf2db3975cad57
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-08-02 08:09:50 +0100

    Fix the app store notification issue (issue #1)

--- Billing.Server.AppStore/AppStoreHookInterceptor.cs
+++ Billing.Server.AppStore/AppStoreHookInterceptor.cs
@@ -1,94 +1,98 @@
 ﻿namespace Zebble.Billing
 {
-    using System;
-    using System.Threading.Tasks;
-    using Microsoft.Extensions.Logging;
-    using Microsoft.Extensions.Options;
-    using Olive;
+	using System;
+	using System.Threading.Tasks;
+	using Microsoft.Extensions.Logging;
+	using Microsoft.Extensions.Options;
+	using Olive;
 
-    class AppStoreHookInterceptor
-    {
-        readonly ILogger<AppStoreHookInterceptor> Logger;
-        readonly AppStoreOptions Options;
-        readonly ISubscriptionRepository Repository;
-        readonly AppStoreConnector StoreConnector;
+	class AppStoreHookInterceptor
+	{
+		readonly ILogger<AppStoreHookInterceptor> Logger;
+		readonly AppStoreOptions Options;
+		readonly ISubscriptionRepository Repository;
+		readonly AppStoreConnector StoreConnector;
 
-        public AppStoreHookInterceptor(ILogger<AppStoreHookInterceptor> logger, IOptionsSnapshot<AppStoreOptions> options, ISubscriptionRepository repository, AppStoreConnector storeConnector)
-        {
-            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            Options = options.Value ?? throw new ArgumentNullException(nameof(options));
-            Repository = repository ?? throw new ArgumentNullException(nameof(repository));
-            StoreConnector = storeConnector ?? throw new ArgumentNullException(nameof(storeConnector));
-        }
+		public AppStoreHookInterceptor(ILogger<AppStoreHookInterceptor> logger, IOptionsSnapshot<AppStoreOptions> options, ISubscriptionRepository repository, AppStoreConnector storeConnector)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			Options = options.Value ?? throw new ArgumentNullException(nameof(options));
+			Repository = repository ?? throw new ArgumentNullException(nameof(repository));
+			StoreConnector = storeConnector ?? throw new ArgumentNullException(nameof(storeConnector));
+		}
 
-        public async Task Intercept(AppStoreNotification notification)
-        {
-            try
-            {
-                ValidateNotification(notification);
+		public async Task Intercept(AppStoreNotification notification)
+		{
+			try
+			{
+				ValidateNotification(notification);
 
-                var subscription = await Repository.GetByPurchaseToken(notification.PurchaseToken);
+				var subscription = await Repository.GetByPurchaseToken(notification.PurchaseToken);
 
-                if (subscription is null)
-                {
-                    var subscriptionInfo = await StoreConnector.GetSubscriptionInfo(notification.ToArgs());
-                    if (subscriptionInfo.Status != SubscriptionQueryStatus.Succeeded) return;
+				if (subscription is null)
+				{
+					var subscriptionInfo = await StoreConnector.GetSubscriptionInfo(notification.ToArgs());
+					if (subscriptionInfo.Status != SubscriptionQueryStatus.Succeeded) return;
 
-                    subscription = await Repository.AddSubscription(new Subscription
-                    {
-                        Id = Guid.NewGuid().ToString(),
-                        ProductId = notification.ProductId,
-                        UserId = subscriptionInfo.UserId.Or("<NOT_PROVIDED>"),
-                        Platform = "AppStore",
-                        TransactionId = subscriptionInfo.TransactionId,
-                        PurchaseToken = notification.PurchaseToken,
-                        TransactionDate = notification.PurchaseDate,
-                        SubscriptionDate = subscriptionInfo.SubscriptionDate,
-                        ExpirationDate = subscriptionInfo.ExpirationDate,
-                        CancellationDate = subscriptionInfo.CancellationDate,
-                        LastUpdate = LocalTime.UtcNow,
-                        AutoRenews = subscriptionInfo.AutoRenews
-                    });
-                }
+					var oldSubscription = await Repository.GetByTransactionId(subscriptionInfo.TransactionId);
 
-                if (notification.Type.IsAnyOf(AppStoreNotificationType.InitialBuy, AppStoreNotificationType.InteractivelyRenewed))
-                {
-                    subscription.SubscriptionDate = notification.PurchaseDate;
-                    subscription.ExpirationDate = notification.ExpirationDate;
-                    subscription.CancellationDate = notification.CancellationDate;
-                    subscription.AutoRenews = notification.AutoRenewStatus;
-                }
-                else if (notification.Type.IsAnyOf(AppStoreNotificationType.CanceledOrUpgraded, AppStoreNotificationType.Refunded, AppStoreNotificationType.FamilySharingRevoked))
-                    subscription.CancellationDate = notification.CancellationDate;
-                else if (notification.Type.IsAnyOf(AppStoreNotificationType.AutoRenewed, AppStoreNotificationType.AutoRecovered))
-                    subscription.ExpirationDate = notification.ExpirationDate;
-                else if (notification.Type == AppStoreNotificationType.AutoRenewFailed)
-                    subscription.ExpirationDate = notification.IsInBillingRetryPeriod == true ? notification.GracePeriodExpirationDate : LocalTime.UtcNow;
-                else if (notification.Type == AppStoreNotificationType.RenewalStatusChanged)
-                    subscription.AutoRenews = notification.AutoRenewStatus;
+					subscription = await Repository.AddSubscription(new Subscription
+					{
+						Id = Guid.NewGuid().ToString(),
+						ProductId = notification.ProductId,
+						UserId = subscriptionInfo.UserId.Or(oldSubscription.UserId).Or("<NOT_PROVIDED>"),
+						Platform = "AppStore",
+						TransactionId = subscriptionInfo.TransactionId,
+						PurchaseToken = notification.PurchaseToken,
+						TransactionDate = notification.PurchaseDate,
+						SubscriptionDate = subscriptionInfo.SubscriptionDate,
+						ExpirationDate = subscriptionInfo.ExpirationDate,
+						CancellationDate = subscriptionInfo.CancellationDate,
+						LastUpdate = LocalTime.UtcNow,
+						AutoRenews = subscriptionInfo.AutoRenews
+					});
+				}
 
-                await Repository.UpdateSubscription(subscription);
+				if (notification.Type.IsAnyOf(AppStoreNotificationType.InitialBuy, AppStoreNotificationType.InteractivelyRenewed))
+				{
+					subscription.SubscriptionDate = notification.PurchaseDate;
+					subscription.ExpirationDate = notification.ExpirationDate;
+					subscription.CancellationDate = notification.CancellationDate;
+					subscription.AutoRenews = notification.AutoRenewStatus;
+				}
+				else if (notification.Type.IsAnyOf(AppStoreNotificationType.CanceledOrUpgraded, AppStoreNotificationType.Refunded, AppStoreNotificationType.FamilySharingRevoked))
+					subscription.CancellationDate = notification.CancellationDate;
+				else if (notification.Type.IsAnyOf(AppStoreNotificationType.AutoRenewed, AppStoreNotificationType.AutoRecovered))
+					subscription.ExpirationDate = notification.ExpirationDate;
+				else if (notification.Type == AppStoreNotificationType.AutoRenewFailed)
+					subscription.ExpirationDate = notification.IsInBillingRetryPeriod == true ? notification.GracePeriodExpirationDate : LocalTime.UtcNow;
+				else if (notification.Type == AppStoreNotificationType.RenewalStatusChanged)
+					subscription.AutoRenews = notification.AutoRenewStatus;
 
-                await Repository.AddTransaction(new Transaction
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    SubscriptionId = subscription.Id,
-                    Platform = "AppStore",
-                    Date = LocalTime.UtcNow,
-                    Details = notification.OriginalData
-                });
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex, $"Failed to intercept following notification. {notification.OriginalData}");
-            }
-        }
+				subscription.LastUpdate = LocalTime.UtcNow;
 
-        void ValidateNotification(AppStoreNotification notification)
-        {
-            if (notification.SharedSecret != Options.SharedSecret) throw new Exception("SharedSecret doesn't match.");
+				await Repository.UpdateSubscription(subscription);
 
-            if (notification.Environment != Options.Environment) throw new Exception("Environment doesn't match.");
-        }
-    }
+				await Repository.AddTransaction(new Transaction
+				{
+					Id = Guid.NewGuid().ToString(),
+					SubscriptionId = subscription.Id,
+					Platform = "AppStore",
+					Date = LocalTime.UtcNow,
+					Details = notification.OriginalData
+				});
+			}
+			catch (Exception ex)
+			{
+				Logger.LogError(ex, $"Failed to intercept following notification. {notification.OriginalData}");
+			}
+		}
+
+		void ValidateNotification(AppStoreNotification notification)
+		{
+			if (notification.SharedSecret != Options.SharedSecret) throw new Exception("SharedSecret doesn't match.");
+
+			if (notification.Environment != Options.Environment) throw new Exception("Environment doesn't match.");
+		}
+	}
 }
--- Billing.Server.EntityFramework/Data/SubscriptionRepository.cs
+++ Billing.Server.EntityFramework/Data/SubscriptionRepository.cs
@@ -1,60 +1,60 @@
 ﻿namespace Zebble.Billing
 {
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Microsoft.EntityFrameworkCore;
-    using Olive;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Microsoft.EntityFrameworkCore;
+	using Olive;
 
-    class SubscriptionRepository : ISubscriptionRepository
-    {
-        readonly SubscriptionDbContext Context;
+	class SubscriptionRepository : ISubscriptionRepository
+	{
+		readonly SubscriptionDbContext Context;
 
-        public SubscriptionRepository(SubscriptionDbContext context) => Context = context;
+		public SubscriptionRepository(SubscriptionDbContext context) => Context = context;
 
-        public Task<Subscription> GetByTransactionId(string transactionId)
-        {
-            return Context.Subscriptions.SingleOrDefaultAsync(x => x.TransactionId == transactionId);
-        }
+		public Task<Subscription> GetByTransactionId(string transactionId)
+		{
+			return Context.Subscriptions.FirstOrDefaultAsync(x => x.TransactionId == transactionId);
+		}
 
-        public Task<Subscription> GetByPurchaseToken(string purchaseToken)
-        {
-            return Context.Subscriptions.SingleOrDefaultAsync(x => x.PurchaseToken == purchaseToken);
-        }
+		public Task<Subscription> GetByPurchaseToken(string purchaseToken)
+		{
+			return Context.Subscriptions.SingleOrDefaultAsync(x => x.PurchaseToken == purchaseToken);
+		}
 
-        public Task<Subscription[]> GetAll(string userId)
-        {
-            return Context.Subscriptions.Where(x => x.UserId == userId)
-                                        .ToArrayAsync();
-        }
+		public Task<Subscription[]> GetAll(string userId)
+		{
+			return Context.Subscriptions.Where(x => x.UserId == userId)
+										.ToArrayAsync();
+		}
 
-        public async Task<Subscription> AddSubscription(Subscription subscription)
-        {
-            await Context.Subscriptions.AddAsync(subscription);
-            await Context.SaveChangesAsync();
+		public async Task<Subscription> AddSubscription(Subscription subscription)
+		{
+			await Context.Subscriptions.AddAsync(subscription);
+			await Context.SaveChangesAsync();
 
-            return subscription;
-        }
+			return subscription;
+		}
 
-        public async Task UpdateSubscription(Subscription subscription)
-        {
-            Context.Subscriptions.Update(subscription);
-            await Context.SaveChangesAsync();
-        }
+		public async Task UpdateSubscription(Subscription subscription)
+		{
+			Context.Subscriptions.Update(subscription);
+			await Context.SaveChangesAsync();
+		}
 
-        public async Task<Transaction> AddTransaction(Transaction transaction)
-        {
-            await Context.Transactions.AddAsync(transaction);
-            await Context.SaveChangesAsync();
+		public async Task<Transaction> AddTransaction(Transaction transaction)
+		{
+			await Context.Transactions.AddAsync(transaction);
+			await Context.SaveChangesAsync();
 
-            return transaction;
-        }
+			return transaction;
+		}
 
-        public async Task<string> GetOriginUserOfTransactionId(string transactionId)
-        {
-            var subscriptions = await Context.Subscriptions.Where(x => x.TransactionId == transactionId)
-                                                           .ToListAsync();
+		public async Task<string> GetOriginUserOfTransactionId(string transactionId)
+		{
+			var subscriptions = await Context.Subscriptions.Where(x => x.TransactionId == transactionId)
+														   .ToListAsync();
 
-            return subscriptions.Select(x => x.UserId).FirstOrDefault(x => x.HasValue());
-        }
-    }
+			return subscriptions.Select(x => x.UserId).FirstOrDefault(x => x.HasValue());
+		}
+	}
 }
