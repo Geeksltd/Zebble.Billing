48175c20bc4ba61011b914b2da14cad67096864b
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-08-03 16:35:43 +0100

    Improve performance

--- Billing.Server.DynamoDb/Data/BillingDynamoDbContext.cs
+++ Billing.Server.DynamoDb/Data/BillingDynamoDbContext.cs
@@ -1,80 +1,63 @@
 ﻿namespace Zebble.Billing
 {
-    using Amazon.DynamoDBv2;
-    using Amazon.DynamoDBv2.DataModel;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using System.Linq;
-    using Olive;
-    using System;
-    using System.Linq.Expressions;
+	using Amazon.DynamoDBv2;
+	using Amazon.DynamoDBv2.DataModel;
+	using System.Threading.Tasks;
+	using System.Linq;
+	using Olive;
+	using System;
+	using System.Linq.Expressions;
 
-    public abstract class BillingDynamoDbContext : DynamoDBContext
-    {
-        protected BillingDynamoDbContext(IAmazonDynamoDB client) : base(client) { }
+	public abstract class BillingDynamoDbContext : DynamoDBContext
+	{
+		protected BillingDynamoDbContext(IAmazonDynamoDB client) : base(client) { }
 
-        protected DbIndex<T> Index<T>(string name) => new(this, name);
+		protected DbIndex<T> Index<T>(string name) => new(this, name);
 
-        protected DbTable<T> Table<T>() => new(this);
+		protected DbTable<T> Table<T>() => new(this);
 
-        public class DbIndex<T>
-        {
-            readonly IDynamoDBContext Db;
-            readonly DynamoDBOperationConfig Config;
+		public class DbIndex<T>
+		{
+			readonly IDynamoDBContext Db;
+			readonly DynamoDBOperationConfig Config;
 
-            public DbIndex(IDynamoDBContext db, string indexName)
-            {
-                Db = db;
-                Config = new DynamoDBOperationConfig { IndexName = indexName };
-            }
+			public DbIndex(IDynamoDBContext db, string indexName)
+			{
+				Db = db;
+				Config = new DynamoDBOperationConfig { IndexName = indexName };
+			}
 
-            public async Task<T[]> All(object hashKey)
-            {
-                var search = Db.QueryAsync<T>(hashKey, Config);
-                return (await search.GetRemainingAsync())?.ToArray() ?? new T[0];
-            }
+			public async Task<T[]> All(object hashKey)
+			{
+				var search = Db.QueryAsync<T>(hashKey, Config);
+				return (await search.GetRemainingAsync())?.ToArray() ?? new T[0];
+			}
 
-            public Task<T> FirstOrDefault(string hashKey) => All(hashKey).FirstOrDefault();
-        }
+			public Task<T> FirstOrDefault(string hashKey) => All(hashKey).FirstOrDefault();
+		}
 
-        public class DbTable<T>
-        {
-            readonly IDynamoDBContext Db;
+		public class DbTable<T>
+		{
+			readonly IDynamoDBContext Db;
 
-            public DbTable(IDynamoDBContext db) => Db = db;
+			public DbTable(IDynamoDBContext db) => Db = db;
 
-            public Task<List<T>> All(params ScanCondition[] conditions) => Db.ScanAsync<T>(conditions).GetRemainingAsync();
+			public Task AddAsync(T newRecord) => Db.SaveAsync(newRecord);
 
-            public async Task<T> Load(string hash) => await Db.LoadAsync<T>(hash);
+			public async Task UpdateAsync(Expression<Func<T, string>> hashSelector, T updatedRecord)
+			{
+				var hash = (string)Expression.Invoke(hashSelector, Expression.Constant(updatedRecord)).CompileAndInvoke();
+				var hashName = ((MemberExpression)hashSelector.Body).Member.Name;
 
-            public async Task<T> FirstOrDefault(params ScanCondition[] conditions)
-            {
-                var scan = Db.ScanAsync<T>(conditions);
+				var dbRecord = await Load(hash);
 
-                try
-                {
-                    return (await scan.GetNextSetAsync()).FirstOrDefault();
-                }
-                catch
-                {
-                    return default;
-                }
-            }
+				if (dbRecord is null) dbRecord = updatedRecord;
+				else ReflectionExtensions.CopyPropertiesFrom(dbRecord, updatedRecord, hashName);
 
-            public Task AddAsync(T newRecord) => Db.SaveAsync(newRecord);
+				await Db.SaveAsync(dbRecord);
+			}
 
-            public async Task UpdateAsync(Expression<Func<T, string>> hashSelector, T updatedRecord)
-            {
-                var hash = (string)Expression.Invoke(hashSelector, Expression.Constant(updatedRecord)).CompileAndInvoke();
-                var hashName = ((MemberExpression)hashSelector.Body).Member.Name;
-
-                var dbRecord = await Load(hash);
-
-                if (dbRecord is null) dbRecord = updatedRecord;
-                else ReflectionExtensions.CopyPropertiesFrom(dbRecord, updatedRecord, hashName);
-
-                await Db.SaveAsync(dbRecord);
-            }
-        }
-    }
+			async Task<T> Load(string hash) => await Db.LoadAsync<T>(hash);
+		}
+	}
 }
--- Billing.Server.DynamoDb/Data/SubscriptionRepository.cs
+++ Billing.Server.DynamoDb/Data/SubscriptionRepository.cs
@@ -1,58 +1,57 @@
 ﻿namespace Zebble.Billing
 {
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Amazon.DynamoDBv2.DataModel;
-    using Amazon.DynamoDBv2.DocumentModel;
-    using Olive;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Amazon.DynamoDBv2.DataModel;
+	using Amazon.DynamoDBv2.DocumentModel;
+	using Olive;
 
-    class SubscriptionRepository : ISubscriptionRepository
-    {
-        readonly SubscriptionDbContext Context;
+	class SubscriptionRepository : ISubscriptionRepository
+	{
+		readonly SubscriptionDbContext Context;
 
-        public SubscriptionRepository(SubscriptionDbContext context) => Context = context;
+		public SubscriptionRepository(SubscriptionDbContext context) => Context = context;
 
-        public async Task<Subscription> GetByTransactionId(string transactionId)
-        {
-            return await Context.SubscriptionTransactions.FirstOrDefault(transactionId);
-        }
+		public async Task<Subscription> GetByTransactionId(string transactionId)
+		{
+			return await Context.SubscriptionTransactions.FirstOrDefault(transactionId);
+		}
 
-        public async Task<Subscription> GetByPurchaseToken(string purchaseToken)
-        {
-            var hash = purchaseToken.ToSimplifiedSHA1Hash();
-            return await Context.SubscriptionPurchaseTokenHashes.FirstOrDefault(hash);
-        }
+		public async Task<Subscription> GetByPurchaseToken(string purchaseToken)
+		{
+			var hash = purchaseToken.ToSimplifiedSHA1Hash();
+			return await Context.SubscriptionPurchaseTokenHashes.FirstOrDefault(hash);
+		}
 
-        public async Task<Subscription[]> GetAll(string userId)
-        {
-            return await Context.SubscriptionUsers.All(userId);
-        }
+		public async Task<Subscription[]> GetAll(string userId)
+		{
+			return await Context.SubscriptionUsers.All(userId);
+		}
 
-        public async Task<Subscription> AddSubscription(Subscription subscription)
-        {
-            await Context.Subscriptions.AddAsync(new SubscriptionProxy(subscription));
+		public async Task<Subscription> AddSubscription(Subscription subscription)
+		{
+			await Context.Subscriptions.AddAsync(new SubscriptionProxy(subscription));
 
-            return subscription;
-        }
+			return subscription;
+		}
 
-        public Task UpdateSubscription(Subscription subscription)
-        {
-            return Context.Subscriptions.UpdateAsync(x => x.Id, new SubscriptionProxy(subscription));
-        }
+		public Task UpdateSubscription(Subscription subscription)
+		{
+			return Context.Subscriptions.UpdateAsync(x => x.Id, new SubscriptionProxy(subscription));
+		}
 
-        public async Task<Transaction> AddTransaction(Transaction transaction)
-        {
-            await Context.Transactions.AddAsync(new TransactionProxy(transaction));
+		public async Task<Transaction> AddTransaction(Transaction transaction)
+		{
+			await Context.Transactions.AddAsync(new TransactionProxy(transaction));
 
-            return transaction;
-        }
+			return transaction;
+		}
 
-        public async Task<string> GetOriginUserOfTransactionId(string transactionId)
-        {
-            var condition = new ScanCondition(nameof(Subscription.TransactionId), ScanOperator.Equal, transactionId);
-            var subscriptions = await Context.Subscriptions.All(condition);
+		public async Task<string> GetOriginUserOfTransactionId(string transactionId)
+		{
+			var subscriptions = await Context.SubscriptionTransactions.All(transactionId);
 
-            return subscriptions.Select(x => x.UserId).FirstOrDefault(x => x.HasValue());
-        }
-    }
+			return subscriptions.Select(x => x.UserId).FirstOrDefault(x => x.HasValue());
+		}
+	}
 }
