39758839b8d39e2880e6203d0fccf6588fec2171
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-08-16 09:42:48 +0100

    Update

--- Billing.Server/SubscriptionManager.cs
+++ Billing.Server/SubscriptionManager.cs
@@ -1,130 +1,143 @@
 ﻿namespace Zebble.Billing
 {
-    using System;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Microsoft.Extensions.Logging;
-    using Olive;
+	using System;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Microsoft.Extensions.Logging;
+	using Olive;
 
-    class SubscriptionManager : ISubscriptionManager
-    {
-        readonly ILogger<SubscriptionManager> Logger;
-        readonly ISubscriptionRepository Repository;
-        readonly IStoreConnectorResolver StoreConnectorResolver;
+	class SubscriptionManager : ISubscriptionManager
+	{
+		readonly ILogger<SubscriptionManager> Logger;
+		readonly ISubscriptionRepository Repository;
+		readonly IStoreConnectorResolver StoreConnectorResolver;
 
-        public SubscriptionManager(ILogger<SubscriptionManager> logger, ISubscriptionRepository repository, IStoreConnectorResolver storeConnectorResolver)
-        {
-            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            Repository = repository ?? throw new ArgumentNullException(nameof(logger));
-            StoreConnectorResolver = storeConnectorResolver ?? throw new ArgumentNullException(nameof(logger));
-        }
+		public SubscriptionManager(ILogger<SubscriptionManager> logger, ISubscriptionRepository repository, IStoreConnectorResolver storeConnectorResolver)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			Repository = repository ?? throw new ArgumentNullException(nameof(logger));
+			StoreConnectorResolver = storeConnectorResolver ?? throw new ArgumentNullException(nameof(logger));
+		}
 
-        public async Task<PurchaseAttemptResult> PurchaseAttempt(string userId, string platform, string productId, string purchaseToken)
-        {
-            var subscription = await Repository.GetByPurchaseToken(purchaseToken);
-            if (subscription is not null)
-            {
-                Logger.LogWarning($"An existing subscription found for token '{purchaseToken}'.");
-                Logger.LogWarning($"Additional params {{ userId: {userId}, platform: {platform}, productId: {productId} }}");
-                Logger.LogWarning($"Existing params {{ userId: {subscription.UserId}, platform: {subscription.Platform}, productId: {subscription.ProductId} }}");
-                throw new Exception($"An existing subscription found for token '{purchaseToken}'.");
-            }
+		public async Task<PurchaseAttemptResult> PurchaseAttempt(string userId, string platform, string productId, string purchaseToken)
+		{
+			var subscription = await Repository.GetByPurchaseToken(purchaseToken);
+			if (subscription is not null)
+			{
+				Logger.LogWarning($"An existing subscription found for token '{purchaseToken}'.");
+				Logger.LogWarning($"Additional params {{ userId: {userId}, platform: {platform}, productId: {productId} }}");
+				Logger.LogWarning($"Existing params {{ userId: {subscription.UserId}, platform: {subscription.Platform}, productId: {subscription.ProductId} }}");
+				throw new Exception($"An existing subscription found for token '{purchaseToken}'.");
+			}
 
-            var storeConnector = StoreConnectorResolver.Resolve(platform);
-            var subscriptionInfo = await storeConnector.GetSubscriptionInfo(new SubscriptionInfoArgs
-            {
-                UserId = userId,
-                ProductId = productId,
-                PurchaseToken = purchaseToken
-            });
+			var storeConnector = StoreConnectorResolver.Resolve(platform);
+			var subscriptionInfo = await storeConnector.GetSubscriptionInfo(new SubscriptionInfoArgs
+			{
+				UserId = userId,
+				ProductId = productId,
+				PurchaseToken = purchaseToken
+			});
 
-            if (subscriptionInfo.Status == SubscriptionQueryStatus.NotFound)
-            {
-                Logger.LogWarning($"No subscription info found for token '{purchaseToken}'.");
-                Logger.LogWarning($"Additional params {{ userId: {userId}, platform: {platform}, productId: {productId} }}");
-                return PurchaseAttemptResult.Failed;
-            }
+			if (subscriptionInfo.Status == SubscriptionQueryStatus.NotFound)
+			{
+				Logger.LogWarning($"No subscription info found for token '{purchaseToken}'.");
+				Logger.LogWarning($"Additional params {{ userId: {userId}, platform: {platform}, productId: {productId} }}");
+				return PurchaseAttemptResult.Failed;
+			}
 
-            if (await IsSubscriptionMismatched(userId, subscriptionInfo))
-            {
-                Logger.LogWarning($"A mismatched subscription info found for token '{purchaseToken}'.");
-                Logger.LogWarning($"Additional params {{ userId: {userId}, platform: {platform}, productId: {productId} }}");
-                return PurchaseAttemptResult.UserMismatched;
-            }
+			if (await IsSubscriptionMismatched(userId, subscriptionInfo))
+			{
+				Logger.LogWarning($"A mismatched subscription info found for token '{purchaseToken}'.");
+				Logger.LogWarning($"Additional params {{ userId: {userId}, platform: {platform}, productId: {productId} }}");
+				return PurchaseAttemptResult.UserMismatched;
+			}
 
-            await Repository.AddSubscription(new Subscription
-            {
-                Id = Guid.NewGuid().ToString(),
-                UserId = userId,
-                Platform = platform,
-                ProductId = productId,
-                TransactionId = subscriptionInfo.TransactionId,
-                TransactionDate = subscriptionInfo.SubscriptionDate,
-                PurchaseToken = purchaseToken,
-                LastUpdate = LocalTime.UtcNow,
-                SubscriptionDate = subscriptionInfo.SubscriptionDate,
-                ExpirationDate = subscriptionInfo.ExpirationDate,
-                CancellationDate = subscriptionInfo.CancellationDate,
-                AutoRenews = subscriptionInfo.AutoRenews
-            });
+			await Repository.AddSubscription(new Subscription
+			{
+				Id = Guid.NewGuid().ToString(),
+				UserId = userId,
+				Platform = platform,
+				ProductId = productId,
+				TransactionId = subscriptionInfo.TransactionId,
+				TransactionDate = subscriptionInfo.SubscriptionDate,
+				PurchaseToken = purchaseToken,
+				LastUpdate = LocalTime.UtcNow,
+				SubscriptionDate = subscriptionInfo.SubscriptionDate,
+				ExpirationDate = subscriptionInfo.ExpirationDate,
+				CancellationDate = subscriptionInfo.CancellationDate,
+				AutoRenews = subscriptionInfo.AutoRenews
+			});
 
-            return PurchaseAttemptResult.Succeeded;
-        }
+			return PurchaseAttemptResult.Succeeded;
+		}
 
-        public async Task<Subscription> GetSubscriptionStatus(string userId)
-        {
-            var subscriptions = await Repository.GetAll(userId);
-            if (subscriptions.None())
-            {
-                Logger.LogWarning($"Found no subscription record for user with id '{userId}'.");
-                return null;
-            }
+		public async Task<Subscription> GetSubscriptionStatus(string userId)
+		{
+			var subscriptions = await Repository.GetAll(userId);
+			if (subscriptions.None())
+			{
+				Logger.LogWarning($"Found no subscription record for user with id '{userId}'.");
+				return null;
+			}
 
-            Logger.LogInformation($"Found {subscriptions.Length} subscription records for user with id '{userId}'.");
+			Logger.LogInformation($"Found {subscriptions.Length} subscription records for user with id '{userId}'.");
 
-            var subscription = subscriptions.OrderBy(x => x.SubscriptionDate).LastOrDefault();
+			var subscription = FindValidSubscription(subscriptions);
+			if (subscription?.RequiresStoreUpdate() == true)
+				await TryToUpdateSubscription(subscription);
 
-            if (subscription?.IsActive() == false)
-                subscription = subscriptions.OrderBy(x => x.ExpirationDate).LastOrDefault();
+			return subscription;
+		}
 
-            if (subscription?.RequiresStoreUpdate() == true)
-                await TryToUpdateSubscription(subscription);
+		static Subscription FindValidSubscription(Subscription[] subscriptions)
+		{
+			subscriptions = subscriptions.Where(x => x.IsStarted()).ToArray();
 
-            return subscription;
-        }
+			// Prioritize non-expirables
+			var subscription = subscriptions.LastOrDefault(x => x.ExpirationDate == null && !x.IsCanceled());
+			if (subscription is not null) return subscription;
 
-        async Task<bool> IsSubscriptionMismatched(string userId, SubscriptionInfo subscriptionInfo)
-        {
-            if (userId.IsEmpty()) return false;
+			// Then search in expirables
+			subscriptions = subscriptions.Where(x => x.ExpirationDate.HasValue).OrderBy(x => x.ExpirationDate).ToArray();
 
-            if (subscriptionInfo.TransactionId.IsEmpty()) return false;
+			subscription = subscriptions.LastOrDefault(x => x.CancellationDate == null);
+			if (subscription is not null) return subscription;
 
-            var originUserId = await Repository.GetOriginUserOfTransactionId(subscriptionInfo.TransactionId);
-            if (originUserId.IsEmpty()) return false;
-            if (originUserId.Equals(userId, caseSensitive: false)) return false;
+			return subscriptions.OrderBy(x => x.CancellationDate).LastOrDefault();
+		}
 
-            Logger.LogWarning($"Transaction #{subscriptionInfo.TransactionId} is associated to {originUserId} and can't be used for {userId}.");
-            return true;
-        }
+		async Task<bool> IsSubscriptionMismatched(string userId, SubscriptionInfo subscriptionInfo)
+		{
+			if (userId.IsEmpty()) return false;
 
-        async Task TryToUpdateSubscription(Subscription subscription)
-        {
-            var storeConnector = StoreConnectorResolver.Resolve(subscription.Platform);
-            var subscriptionInfo = await storeConnector.GetSubscriptionInfo(subscription.ToArgs());
+			if (subscriptionInfo.TransactionId.IsEmpty()) return false;
 
-            if (subscriptionInfo.Status != SubscriptionQueryStatus.Succeeded)
-            {
-                Logger.LogWarning($"Subscription with id '{subscription.Id}' needed to be updated, but we were not able to pull fresh data from the store.");
-                Logger.LogWarning($"Additional params {{ userId: {subscription.UserId}, platform: {subscription.Platform}, productId: {subscription.ProductId} }}");
-                return;
-            }
+			var originUserId = await Repository.GetOriginUserOfTransactionId(subscriptionInfo.TransactionId);
+			if (originUserId.IsEmpty()) return false;
+			if (originUserId.Equals(userId, caseSensitive: false)) return false;
 
-            subscription.SubscriptionDate = subscriptionInfo.SubscriptionDate;
-            subscription.ExpirationDate = subscriptionInfo.ExpirationDate;
-            subscription.CancellationDate = subscriptionInfo.CancellationDate;
-            subscription.AutoRenews = subscriptionInfo.AutoRenews;
+			Logger.LogWarning($"Transaction #{subscriptionInfo.TransactionId} is associated to {originUserId} and can't be used for {userId}.");
+			return true;
+		}
 
-            await Repository.UpdateSubscription(subscription);
-        }
-    }
+		async Task TryToUpdateSubscription(Subscription subscription)
+		{
+			var storeConnector = StoreConnectorResolver.Resolve(subscription.Platform);
+			var subscriptionInfo = await storeConnector.GetSubscriptionInfo(subscription.ToArgs());
+
+			if (subscriptionInfo.Status != SubscriptionQueryStatus.Succeeded)
+			{
+				Logger.LogWarning($"Subscription with id '{subscription.Id}' needed to be updated, but we were not able to pull fresh data from the store.");
+				Logger.LogWarning($"Additional params {{ userId: {subscription.UserId}, platform: {subscription.Platform}, productId: {subscription.ProductId} }}");
+				return;
+			}
+
+			subscription.SubscriptionDate = subscriptionInfo.SubscriptionDate;
+			subscription.ExpirationDate = subscriptionInfo.ExpirationDate;
+			subscription.CancellationDate = subscriptionInfo.CancellationDate;
+			subscription.AutoRenews = subscriptionInfo.AutoRenews;
+
+			await Repository.UpdateSubscription(subscription);
+		}
+	}
 }
--- Billing.Shared/Extensions/EntityExtensions.cs
+++ Billing.Shared/Extensions/EntityExtensions.cs
@@ -1,18 +1,16 @@
 ﻿namespace Zebble.Billing
 {
-    using System;
-    using Olive;
+	using System;
+	using Olive;
 
-    public static partial class EntityExtensions
-    {
-        public static bool IsActive(this Subscription @this) => @this.IsStarted() && !@this.IsExpired() && !@this.IsCanceled();
+	public static partial class EntityExtensions
+	{
+		public static bool IsStarted(this Subscription @this) => IsInThePast(@this?.SubscriptionDate);
 
-        public static bool IsStarted(this Subscription @this) => IsInThePast(@this?.SubscriptionDate);
+		public static bool IsExpired(this Subscription @this) => IsInThePast(@this?.ExpirationDate);
 
-        public static bool IsExpired(this Subscription @this) => IsInThePast(@this?.ExpirationDate);
+		public static bool IsCanceled(this Subscription @this) => IsInThePast(@this?.CancellationDate);
 
-        public static bool IsCanceled(this Subscription @this) => IsInThePast(@this?.CancellationDate);
-
-        static bool IsInThePast(DateTime? @this) => @this?.ToLocal().IsInThePast() ?? false;
-    }
+		static bool IsInThePast(DateTime? @this) => @this?.ToLocal().IsInThePast() ?? false;
+	}
 }
