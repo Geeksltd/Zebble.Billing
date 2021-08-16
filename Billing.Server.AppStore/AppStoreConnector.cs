a1dd76d7d741316cb34c7c1e50f98efb37fc6d1c
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-08-16 13:27:25 +0100

    Fix null ref error

--- Billing.Server.AppStore/AppStoreConnector.cs
+++ Billing.Server.AppStore/AppStoreConnector.cs
@@ -1,199 +1,203 @@
 ﻿namespace Zebble.Billing
 {
-    using System;
-    using System.Collections.ObjectModel;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Apple.Receipt.Models;
-    using Apple.Receipt.Models.Enums;
-    using Apple.Receipt.Verificator.Models;
-    using Apple.Receipt.Verificator.Models.IAPVerification;
-    using Apple.Receipt.Verificator.Services;
-    using Microsoft.Extensions.Logging;
-    using Microsoft.Extensions.Options;
-    using Newtonsoft.Json;
-    using Olive;
+	using System;
+	using System.Collections.ObjectModel;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Apple.Receipt.Models;
+	using Apple.Receipt.Models.Enums;
+	using Apple.Receipt.Verificator.Models;
+	using Apple.Receipt.Verificator.Models.IAPVerification;
+	using Apple.Receipt.Verificator.Services;
+	using Microsoft.Extensions.Logging;
+	using Microsoft.Extensions.Options;
+	using Newtonsoft.Json;
+	using Olive;
 
-    class AppStoreConnector : IStoreConnector
-    {
-        readonly ILogger<AppStoreConnector> Logger;
-        readonly IAppleReceiptVerificatorService Verificator;
-        readonly IOptionsSnapshot<AppleReceiptVerificationSettings> Settings;
+	class AppStoreConnector : IStoreConnector
+	{
+		readonly ILogger<AppStoreConnector> Logger;
+		readonly IAppleReceiptVerificatorService Verificator;
+		readonly IOptionsSnapshot<AppleReceiptVerificationSettings> Settings;
 
-        public AppStoreConnector(
-            ILogger<AppStoreConnector> logger,
-            IAppleReceiptVerificatorService verificator,
-            IOptionsSnapshot<AppleReceiptVerificationSettings> settings
-        )
-        {
-            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            Verificator = verificator ?? throw new ArgumentNullException(nameof(verificator));
-            Settings = settings ?? throw new ArgumentNullException(nameof(settings));
-        }
+		public AppStoreConnector(
+			ILogger<AppStoreConnector> logger,
+			IAppleReceiptVerificatorService verificator,
+			IOptionsSnapshot<AppleReceiptVerificationSettings> settings
+		)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			Verificator = verificator ?? throw new ArgumentNullException(nameof(verificator));
+			Settings = settings ?? throw new ArgumentNullException(nameof(settings));
+		}
 
-        public async Task<SubscriptionInfo> GetSubscriptionInfo(SubscriptionInfoArgs args)
-        {
-            var (result, status) = await GetVerifiedResult(args.PurchaseToken);
+		public async Task<SubscriptionInfo> GetSubscriptionInfo(SubscriptionInfoArgs args)
+		{
+			var (result, status) = await GetVerifiedResult(args.PurchaseToken);
 
-            return status switch
-            {
-                SubscriptionQueryStatus.NotFound => SubscriptionInfo.NotFound,
-                SubscriptionQueryStatus.Expired => CreateExpiredSubscription(args.UserId, result.AppleVerificationResponse),
-                _ => CreateSubscription(args.UserId, args.ProductId, result.AppleVerificationResponse)
-            };
-        }
+			return status switch
+			{
+				SubscriptionQueryStatus.NotFound => SubscriptionInfo.NotFound,
+				SubscriptionQueryStatus.Expired => CreateExpiredSubscription(args.UserId, result.AppleVerificationResponse),
+				_ => CreateSubscription(args.UserId, args.ProductId, result.AppleVerificationResponse)
+			};
+		}
 
-        static SubscriptionInfo CreateExpiredSubscription(string userId, IAPVerificationResponse response)
-        {
-            return new SubscriptionInfo
-            {
-                UserId = userId,
-                SubscriptionDate = response.Receipt.OriginalPurchaseDateDt,
-                ExpirationDate = LocalTime.UtcNow
-            };
-        }
+		static SubscriptionInfo CreateExpiredSubscription(string userId, IAPVerificationResponse response)
+		{
+			return new SubscriptionInfo
+			{
+				UserId = userId,
+				SubscriptionDate = response.Receipt.OriginalPurchaseDateDt,
+				ExpirationDate = LocalTime.UtcNow
+			};
+		}
 
-        SubscriptionInfo CreateSubscription(string userId, string productId, IAPVerificationResponse response)
-        {
-            var purchase = response?.LatestReceiptInfo?.OrderBy(x => x.PurchaseDateDt).LastOrDefault(x => x.ProductId == productId);
-            if (purchase is null)
-            {
-                Logger.LogError($"The receipt contains no purchase info for product id '{productId}'.");
-                return SubscriptionInfo.NotFound;
-            }
+		SubscriptionInfo CreateSubscription(string userId, string productId, IAPVerificationResponse response)
+		{
+			var purchase = response?.LatestReceiptInfo?.OrderBy(x => x.PurchaseDateDt).LastOrDefault(x => x.ProductId == productId);
+			if (purchase is null)
+			{
+				Logger.LogError($"The receipt contains no purchase info for product id '{productId}'.");
+				return SubscriptionInfo.NotFound;
+			}
 
-            return new SubscriptionInfo
-            {
-                UserId = userId,
-                TransactionId = purchase.OriginalTransactionId,
-                SubscriptionDate = purchase.PurchaseDateDt,
-                ExpirationDate = purchase.ExpirationDateDt,
-                CancellationDate = purchase.CancellationDateDt,
-                AutoRenews = purchase.SubscriptionAutoRenewStatus == AppleSubscriptionAutoRenewStatus.Active
-            };
-        }
+			return new SubscriptionInfo
+			{
+				UserId = userId,
+				TransactionId = purchase.OriginalTransactionId,
+				SubscriptionDate = purchase.PurchaseDateDt,
+				ExpirationDate = purchase.ExpirationDateDt,
+				CancellationDate = purchase.CancellationDateDt,
+				AutoRenews = purchase.SubscriptionAutoRenewStatus == AppleSubscriptionAutoRenewStatus.Active
+			};
+		}
 
-        async Task<(AppleReceiptVerificationResult, SubscriptionQueryStatus)> GetVerifiedResult(string purchaseToken)
-        {
-            var result = await Verificator.VerifyAppleReceiptAsync(purchaseToken);
+		async Task<(AppleReceiptVerificationResult, SubscriptionQueryStatus)> GetVerifiedResult(string purchaseToken)
+		{
+			var result = await Verificator.VerifyAppleReceiptAsync(purchaseToken);
 
-            if (result?.Status == IAPVerificationResponseStatus.TestReceiptOnProd)
-                result = await Verificator.VerifyAppleSandBoxReceiptAsync(purchaseToken);
-            else if (result?.Status == IAPVerificationResponseStatus.ProdReceiptOnTest)
-                result = await Verificator.VerifyAppleProductionReceiptAsync(purchaseToken);
+			if (result?.Status == IAPVerificationResponseStatus.TestReceiptOnProd)
+				result = await Verificator.VerifyAppleSandBoxReceiptAsync(purchaseToken);
+			else if (result?.Status == IAPVerificationResponseStatus.ProdReceiptOnTest)
+				result = await Verificator.VerifyAppleProductionReceiptAsync(purchaseToken);
 
-            if (result?.AppleVerificationResponse?.LatestReceiptInfo == null)
-            {
-                result = await GetLegacyResponse(Settings.Value.VerifyUrl, purchaseToken);
+			if (result?.AppleVerificationResponse?.LatestReceiptInfo == null)
+			{
+				result = await GetLegacyResponse(Settings.Value.VerifyUrl, purchaseToken);
 
-                if (result?.Status == IAPVerificationResponseStatus.TestReceiptOnProd)
-                    result = await GetLegacyResponse(Settings.Value.SandboxUrl, purchaseToken);
-                else if (result?.Status == IAPVerificationResponseStatus.ProdReceiptOnTest)
-                    result = await GetLegacyResponse(Settings.Value.ProductionUrl, purchaseToken);
-            }
+				if (result?.Status == IAPVerificationResponseStatus.TestReceiptOnProd)
+					result = await GetLegacyResponse(Settings.Value.SandboxUrl, purchaseToken);
+				else if (result?.Status == IAPVerificationResponseStatus.ProdReceiptOnTest)
+					result = await GetLegacyResponse(Settings.Value.ProductionUrl, purchaseToken);
+			}
 
-            if (result is null) return (null, SubscriptionQueryStatus.NotFound);
+			if (result is null) return (null, SubscriptionQueryStatus.NotFound);
 
-            return (result, ValidateVerificationResult(result));
-        }
+			return (result, ValidateVerificationResult(result));
+		}
 
-        async Task<AppleReceiptVerificationResult> GetLegacyResponse(string url, string purchaseToken)
-        {
-            var response = "(null)";
+		async Task<AppleReceiptVerificationResult> GetLegacyResponse(string url, string purchaseToken)
+		{
+			var response = "(null)";
 
-            try
-            {
-                var request = new IAPVerificationRequest(purchaseToken, Settings.Value.VerifyReceiptSharedSecret);
-                response = await url.AsUri().PostJson(request);
-                var result = JsonConvert.DeserializeObject<IAPLegacyVerificationResult>(response);
+			try
+			{
+				var request = new IAPVerificationRequest(purchaseToken, Settings.Value.VerifyReceiptSharedSecret);
+				response = await url.AsUri().PostJson(request);
+				var result = JsonConvert.DeserializeObject<IAPLegacyVerificationResult>(response);
 
-                return new AppleReceiptVerificationResult(null, result.Status)
-                {
-                    AppleVerificationResponse = new IAPVerificationResponse
-                    {
-                        Receipt = new AppleAppReceipt
-                        {
-                            OriginalPurchaseDateMs = result.Receipt.OriginalPurchaseDateMs,
-                        },
-                        LatestReceiptInfo = new Collection<AppleInAppPurchaseReceipt>
-                    {
-                        new AppleInAppPurchaseReceipt {
-                            PurchaseDateMs = result.Receipt.PurchaseDateMs,
-                            ProductId = result.Receipt.ProductId,
-                            OriginalTransactionId = result.Receipt.OriginalTransactionId,
-                            ExpirationDateMs = result.Receipt.ExpiresDateMs,
-                        }
-                    }
-                    }
-                };
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex, $"Failed to verify legacy receipt. Url: {url}, Response: {response}");
-                throw;
-            }
-        }
+				var verificationResult = new AppleReceiptVerificationResult(null, result.Status);
 
-        SubscriptionQueryStatus ValidateVerificationResult(AppleReceiptVerificationResult verificationResult)
-        {
-            var response = verificationResult?.AppleVerificationResponse;
+				if (result.Receipt is not null)
+				{
+					verificationResult.AppleVerificationResponse = new IAPVerificationResponse
+					{
+						Receipt = new AppleAppReceipt
+						{
+							OriginalPurchaseDateMs = result.Receipt.OriginalPurchaseDateMs,
+						},
+						LatestReceiptInfo = new Collection<AppleInAppPurchaseReceipt>
+						{
+							new AppleInAppPurchaseReceipt {
+								PurchaseDateMs = result.Receipt.PurchaseDateMs,
+								ProductId = result.Receipt.ProductId,
+								OriginalTransactionId = result.Receipt.OriginalTransactionId,
+								ExpirationDateMs = result.Receipt.ExpiresDateMs,
+							}
+						}
+					};
+				};
 
-            if (response is null)
-            {
-                Logger.LogWarning($"{verificationResult.Message}.");
-                return SubscriptionQueryStatus.NotFound;
-            }
+				return verificationResult;
+			}
+			catch (Exception ex)
+			{
+				Logger.LogError(ex, $"Failed to verify legacy receipt. Url: {url}, Response: {response}");
+				throw;
+			}
+		}
 
-            if (response.StatusCode != IAPVerificationResponseStatus.Ok)
-            {
-                Logger.LogWarning($"{verificationResult.Message} [{response.Status} - {response.StatusCode}]");
-                if (response.StatusCode == IAPVerificationResponseStatus.SubscriptionExpired) return SubscriptionQueryStatus.Expired;
-                return SubscriptionQueryStatus.NotFound;
-            }
+		SubscriptionQueryStatus ValidateVerificationResult(AppleReceiptVerificationResult verificationResult)
+		{
+			var response = verificationResult?.AppleVerificationResponse;
 
-            return SubscriptionQueryStatus.Succeeded;
-        }
+			if (response is null)
+			{
+				Logger.LogWarning($"{verificationResult.Message}.");
+				return SubscriptionQueryStatus.NotFound;
+			}
 
-        class IAPVerificationRequest
-        {
-            public IAPVerificationRequest(string receiptData, string password)
-            {
-                ReceiptData = receiptData;
-                Password = password;
-            }
+			if (response.StatusCode != IAPVerificationResponseStatus.Ok)
+			{
+				Logger.LogWarning($"{verificationResult.Message} [{response.Status} - {response.StatusCode}]");
+				if (response.StatusCode == IAPVerificationResponseStatus.SubscriptionExpired) return SubscriptionQueryStatus.Expired;
+				return SubscriptionQueryStatus.NotFound;
+			}
 
-            [JsonProperty(PropertyName = "receipt-data")]
-            public string ReceiptData { get; set; }
+			return SubscriptionQueryStatus.Succeeded;
+		}
 
-            [JsonProperty(PropertyName = "password")]
-            public string Password { get; set; }
-        }
+		class IAPVerificationRequest
+		{
+			public IAPVerificationRequest(string receiptData, string password)
+			{
+				ReceiptData = receiptData;
+				Password = password;
+			}
 
-        class IAPLegacyVerificationResult
-        {
-            [JsonProperty(PropertyName = "receipt")]
-            public IAPLegacyReceipt Receipt { get; set; }
+			[JsonProperty(PropertyName = "receipt-data")]
+			public string ReceiptData { get; set; }
 
-            [JsonProperty(PropertyName = "status")]
-            public IAPVerificationResponseStatus Status { get; set; }
-        }
+			[JsonProperty(PropertyName = "password")]
+			public string Password { get; set; }
+		}
 
-        class IAPLegacyReceipt
-        {
-            [JsonProperty("expires_date_ms")]
-            public string ExpiresDateMs { get; set; }
+		class IAPLegacyVerificationResult
+		{
+			[JsonProperty(PropertyName = "receipt")]
+			public IAPLegacyReceipt Receipt { get; set; }
 
-            [JsonProperty("purchase_date_ms")]
-            public string PurchaseDateMs { get; set; }
+			[JsonProperty(PropertyName = "status")]
+			public IAPVerificationResponseStatus Status { get; set; }
+		}
 
-            [JsonProperty("original_purchase_date_ms")]
-            public string OriginalPurchaseDateMs { get; set; }
+		class IAPLegacyReceipt
+		{
+			[JsonProperty("expires_date_ms")]
+			public string ExpiresDateMs { get; set; }
 
-            [JsonProperty("original_transaction_id")]
-            public string OriginalTransactionId { get; set; }
+			[JsonProperty("purchase_date_ms")]
+			public string PurchaseDateMs { get; set; }
 
-            [JsonProperty("product_id")]
-            public string ProductId { get; set; }
-        }
-    }
+			[JsonProperty("original_purchase_date_ms")]
+			public string OriginalPurchaseDateMs { get; set; }
+
+			[JsonProperty("original_transaction_id")]
+			public string OriginalTransactionId { get; set; }
+
+			[JsonProperty("product_id")]
+			public string ProductId { get; set; }
+		}
+	}
 }
