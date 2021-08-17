ef7fdd316643f11c7031a00726ab40362b9d1b00
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-08-17 19:21:19 +0430

    Fix background refresh issue

--- Billing.Plugin/Shared/BillingContext.Refresh.cs
+++ Billing.Plugin/Shared/BillingContext.Refresh.cs
@@ -14,8 +14,11 @@
         public async Task BackgroundRefresh()
         {
             while (User == null) await Task.Delay(500);
-            if (Subscription?.ExpirationDate is null) return;
-            if (Subscription.ExpirationDate?.AddDays(-2) > LocalTime.Now) return;
+            if (Subscription is not null)
+            {
+                if (Subscription.ExpirationDate is null) return;
+                if (Subscription.ExpirationDate?.AddDays(-2) > LocalTime.Now) return;
+            }
 
             await UIContext.AwaitConnection();
             try { await DoRefresh(); }
