b0a270755ccdb12e8fdcfd5ab540c811767a232e
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2021-10-04 20:40:13 +0330

    Fix local/utc date comparison

--- Billing.Shared/Extensions/EntityExtensions.cs
+++ Billing.Shared/Extensions/EntityExtensions.cs
@@ -1,16 +1,21 @@
 ﻿namespace Zebble.Billing
 {
-	using System;
-	using Olive;
+    using System;
+    using Olive;
 
-	public static partial class EntityExtensions
-	{
-		public static bool IsStarted(this Subscription @this) => IsInThePast(@this?.SubscriptionDate);
+    public static partial class EntityExtensions
+    {
+        public static bool IsStarted(this Subscription @this) => IsInThePast(@this?.SubscriptionDate);
 
-		public static bool IsExpired(this Subscription @this) => IsInThePast(@this?.ExpirationDate);
+        public static bool IsExpired(this Subscription @this) => IsInThePast(@this?.ExpirationDate);
 
-		public static bool IsCanceled(this Subscription @this) => IsInThePast(@this?.CancellationDate);
+        public static bool IsCanceled(this Subscription @this) => IsInThePast(@this?.CancellationDate);
 
-		static bool IsInThePast(DateTime? @this) => @this?.ToLocal().IsInThePast() ?? false;
-	}
+        static bool IsInThePast(DateTime? @this)
+        {
+            if (@this is null) return false;
+            if (@this.Value.Kind == DateTimeKind.Local) return @this.Value.IsInThePast();
+            return @this.Value.ToLocal().IsInThePast();
+        }
+    }
 }
