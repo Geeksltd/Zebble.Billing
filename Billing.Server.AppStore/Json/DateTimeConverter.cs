8042d4c64d4cf0a7cb551933c600ca2f4959629b
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2022-02-07 15:49:03 +0330

    Fix the date conversion issue

--- Billing.Server.AppStore/Json/DateTimeConverter.cs
+++ Billing.Server.AppStore/Json/DateTimeConverter.cs
@@ -6,16 +6,16 @@
     using System.Text.Json.Serialization;
     using Olive;
 
-    class DateTimeConverter : JsonConverter<DateTime>
+    class DateTimeConverter : JsonConverter<DateTime?>
     {
-        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             return DateTime.UnixEpoch.AddMilliseconds(reader.GetString().To<long>());
         }
 
-        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(((DateTimeOffset)value).ToUnixTimeMilliseconds().ToString(CultureInfo.InvariantCulture));
+            writer.WriteStringValue(((DateTimeOffset?)value)?.ToUnixTimeMilliseconds().ToString(CultureInfo.InvariantCulture).Or(""));
         }
     }
 }
