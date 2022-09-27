b7fceac7877edd41c43803e43b44a68b0ecd3e2c
Amir H. Jabari <amirh.Jabari@protonmail.com>
2022-09-27 13:49:36 +0330

    add DateOnly properties, to Subscription (#5)

--- Billing.Shared/Subscription.cs
+++ Billing.Shared/Subscription.cs
@@ -7,7 +7,9 @@
         public string ProductId { get; set; }
 
         public DateTime? SubscriptionDate { get; set; }
+        public DateTime? SubscriptionDateOnly => SubscriptionDate?.Date;
         public DateTime? ExpirationDate { get; set; }
         public DateTime? CancellationDate { get; set; }
+        public DateTime? CancellationDateOnly => CancellationDate?.Date;
     }
 }
