7ce669ebb1e0d80771ac335c65b2552bea1d9463
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2023-07-25 14:29:08 +0330

    Store returned dates are in UTC

--- Billing.Shared/Subscription.cs
+++ Billing.Shared/Subscription.cs
@@ -10,24 +10,43 @@
         public DateTime? SubscriptionDate
         {
             get => subscriptionDate;
-            set { subscriptionDate = value; SubscriptionDateOnly = value?.Date; }
+            set
+            {
+                if (value is not null)
+                    value = DateTime.SpecifyKind(value.Value, DateTimeKind.Utc);
+                subscriptionDate = value;
+                SubscriptionDateOnly = value?.Date;
+            }
         }
+
         public DateTime? SubscriptionDateOnly { get; set; }
 
         DateTime? expirationDate;
         public DateTime? ExpirationDate
         {
             get => expirationDate;
-            set { expirationDate = value; ExpirationDateOnly = value?.Date; }
+            set {
+                if (value is not null)
+                    value = DateTime.SpecifyKind(value.Value, DateTimeKind.Utc);
+                expirationDate = value;
+                ExpirationDateOnly = value?.Date;
+            }
         }
+
         public DateTime? ExpirationDateOnly { get; set; }
 
         DateTime? cancellationDate;
         public DateTime? CancellationDate
         {
             get => cancellationDate;
-            set { cancellationDate = value; CancellationDateOnly = value?.Date; }
+            set {
+                if (value is not null)
+                    value = DateTime.SpecifyKind(value.Value, DateTimeKind.Utc);
+                cancellationDate = value;
+                CancellationDateOnly = value?.Date;
+            }
         }
+
         public DateTime? CancellationDateOnly { get; set; }
     }
 }
