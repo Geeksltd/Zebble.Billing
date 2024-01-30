8c4fd6dca4a1a1a91cc99709de2a7c2a6263171d
Mohammad Sadegh Shad <m.sadegh.sh@gmail.com>
2024-01-30 16:59:23 +0330

    Replace the obsolete API

--- Billing.Server.GooglePlay/Extensions/ZebbleBillingServicesBuilderExtensions.cs
+++ Billing.Server.GooglePlay/Extensions/ZebbleBillingServicesBuilderExtensions.cs
@@ -1,83 +1,82 @@
 ﻿namespace Zebble.Billing
 {
-	using Google.Apis.AndroidPublisher.v3;
-	using Google.Apis.Auth.OAuth2;
-	using Google.Apis.Services;
-	using Google.Cloud.PubSub.V1;
-	using Grpc.Auth;
-	using Microsoft.Extensions.Configuration;
-	using Microsoft.Extensions.DependencyInjection;
-	using Microsoft.Extensions.Options;
-	using Olive;
-	using System.Text.Json;
+    using Google.Apis.AndroidPublisher.v3;
+    using Google.Apis.Auth.OAuth2;
+    using Google.Apis.Services;
+    using Google.Cloud.PubSub.V1;
+    using Grpc.Auth;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Options;
+    using Olive;
+    using System.Text.Json;
 
-	public static partial class ZebbleBillingServicesBuilderExtensions
-	{
-		public static ZebbleBillingServicesBuilder AddGooglePlay(this ZebbleBillingServicesBuilder builder, string configKey = "ZebbleBilling:GooglePlay")
-		{
-			builder.Services.AddOptions<GooglePlayOptions>()
-							.Configure<IConfiguration>((opts, config) => config.GetSection(configKey)?.Bind(opts))
-							.Validate(opts => opts.PackageName.HasValue(), $"{nameof(GooglePlayOptions.PackageName)} is empty.")
-							.Validate(opts => opts.Store.ProjectId.HasValue(), $"{nameof(GooglePlayStoreOptions.ProjectId)} is empty.")
-							.Validate(opts => opts.Store.PrivateKeyId.HasValue(), $"{nameof(GooglePlayStoreOptions.PrivateKeyId)} is empty.")
-							.Validate(opts => opts.Store.PrivateKey.HasValue(), $"{nameof(GooglePlayStoreOptions.PrivateKey)} is empty.")
-							.Validate(opts => opts.Store.ClientEmail.HasValue(), $"{nameof(GooglePlayStoreOptions.ClientEmail)} is empty.")
-							.Validate(opts => opts.Store.ClientId.HasValue(), $"{nameof(GooglePlayStoreOptions.ClientId)} is empty.")
-							.Validate(opts => opts.PubSub.ProjectId.HasValue(), $"{nameof(GooglePlayStoreOptions.ProjectId)} is empty.")
-							.Validate(opts => opts.PubSub.PrivateKeyId.HasValue(), $"{nameof(GooglePlayPubSubOptions.PrivateKeyId)} is empty.")
-							.Validate(opts => opts.PubSub.PrivateKey.HasValue(), $"{nameof(GooglePlayPubSubOptions.PrivateKey)} is empty.")
-							.Validate(opts => opts.PubSub.ClientEmail.HasValue(), $"{nameof(GooglePlayPubSubOptions.ClientEmail)} is empty.")
-							.Validate(opts => opts.PubSub.ClientId.HasValue(), $"{nameof(GooglePlayPubSubOptions.ClientId)} is empty.")
-							.Validate(opts => opts.PubSub.SubscriptionId.HasValue(), $"{nameof(GooglePlayPubSubOptions.SubscriptionId)} is empty.")
-							.Validate(opts => opts.QueueProcessorPath.HasValue(), $"{nameof(GooglePlayOptions.QueueProcessorPath)} is empty.");
+    public static partial class ZebbleBillingServicesBuilderExtensions
+    {
+        public static ZebbleBillingServicesBuilder AddGooglePlay(this ZebbleBillingServicesBuilder builder, string configKey = "ZebbleBilling:GooglePlay")
+        {
+            builder.Services.AddOptions<GooglePlayOptions>()
+                            .Configure<IConfiguration>((opts, config) => config.GetSection(configKey)?.Bind(opts))
+                            .Validate(opts => opts.PackageName.HasValue(), $"{nameof(GooglePlayOptions.PackageName)} is empty.")
+                            .Validate(opts => opts.Store.ProjectId.HasValue(), $"{nameof(GooglePlayStoreOptions.ProjectId)} is empty.")
+                            .Validate(opts => opts.Store.PrivateKeyId.HasValue(), $"{nameof(GooglePlayStoreOptions.PrivateKeyId)} is empty.")
+                            .Validate(opts => opts.Store.PrivateKey.HasValue(), $"{nameof(GooglePlayStoreOptions.PrivateKey)} is empty.")
+                            .Validate(opts => opts.Store.ClientEmail.HasValue(), $"{nameof(GooglePlayStoreOptions.ClientEmail)} is empty.")
+                            .Validate(opts => opts.Store.ClientId.HasValue(), $"{nameof(GooglePlayStoreOptions.ClientId)} is empty.")
+                            .Validate(opts => opts.PubSub.ProjectId.HasValue(), $"{nameof(GooglePlayStoreOptions.ProjectId)} is empty.")
+                            .Validate(opts => opts.PubSub.PrivateKeyId.HasValue(), $"{nameof(GooglePlayPubSubOptions.PrivateKeyId)} is empty.")
+                            .Validate(opts => opts.PubSub.PrivateKey.HasValue(), $"{nameof(GooglePlayPubSubOptions.PrivateKey)} is empty.")
+                            .Validate(opts => opts.PubSub.ClientEmail.HasValue(), $"{nameof(GooglePlayPubSubOptions.ClientEmail)} is empty.")
+                            .Validate(opts => opts.PubSub.ClientId.HasValue(), $"{nameof(GooglePlayPubSubOptions.ClientId)} is empty.")
+                            .Validate(opts => opts.PubSub.SubscriptionId.HasValue(), $"{nameof(GooglePlayPubSubOptions.SubscriptionId)} is empty.")
+                            .Validate(opts => opts.QueueProcessorPath.HasValue(), $"{nameof(GooglePlayOptions.QueueProcessorPath)} is empty.");
 
-			builder.Services.AddSingleton(sp =>
-			{
-				var options = sp.GetService<IOptions<GooglePlayOptions>>().Value.Store;
-				var initializer = new ServiceAccountCredential(new ServiceAccountCredential.Initializer(options.ClientEmail)
-				{
-					ProjectId = options.ProjectId,
-					KeyId = options.PrivateKeyId,
-					Scopes = new[] { AndroidPublisherService.ScopeConstants.Androidpublisher }
-				}.FromPrivateKey(options.PrivateKey));
+            builder.Services.AddSingleton(sp =>
+            {
+                var options = sp.GetService<IOptions<GooglePlayOptions>>().Value.Store;
+                var initializer = new ServiceAccountCredential(new ServiceAccountCredential.Initializer(options.ClientEmail)
+                {
+                    ProjectId = options.ProjectId,
+                    KeyId = options.PrivateKeyId,
+                    Scopes = new[] { AndroidPublisherService.ScopeConstants.Androidpublisher }
+                }.FromPrivateKey(options.PrivateKey));
 
-				return new AndroidPublisherService(new BaseClientService.Initializer
-				{
-					HttpClientInitializer = initializer
-				});
-			});
+                return new AndroidPublisherService(new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = initializer
+                });
+            });
 
-			builder.Services.AddTransient(sp =>
-			{
-				var options = sp.GetService<IOptions<GooglePlayOptions>>().Value.PubSub;
+            builder.Services.AddTransient(sp =>
+            {
+                var options = sp.GetService<IOptions<GooglePlayOptions>>().Value.PubSub;
 
-				var name = new SubscriptionName(options.ProjectId, options.SubscriptionId);
-				var json = new JsonCredentialParameters
-				{
-					Type = JsonCredentialParameters.ServiceAccountCredentialType,
-					ProjectId = options.ProjectId,
-					PrivateKeyId = options.PrivateKeyId,
-					PrivateKey = options.PrivateKey,
-					ClientEmail = options.ClientEmail,
-					ClientId = options.ClientId
-				}.ToJson(new JsonSerializerOptions { PropertyNamingPolicy = new SnakeCasePropertyNamingPolicy() });
+                var json = new JsonCredentialParameters
+                {
+                    Type = JsonCredentialParameters.ServiceAccountCredentialType,
+                    ProjectId = options.ProjectId,
+                    PrivateKeyId = options.PrivateKeyId,
+                    PrivateKey = options.PrivateKey,
+                    ClientEmail = options.ClientEmail,
+                    ClientId = options.ClientId
+                }.ToJson(new JsonSerializerOptions { PropertyNamingPolicy = new SnakeCasePropertyNamingPolicy() });
 
-				var settings = new SubscriberClient.ClientCreationSettings(
-					credentials: GoogleCredential.FromJson(json).ToChannelCredentials()
-				);
+                return new SubscriberClientBuilder
+                {
+                    ChannelCredentials = GoogleCredential.FromJson(json).ToChannelCredentials(),
+                    SubscriptionName = new SubscriptionName(options.ProjectId, options.SubscriptionId)
+                }.Build(sp);
+            });
 
-				return SubscriberClient.Create(name, settings);
-			});
+            builder.Services.AddStoreConnector<GooglePlayConnector>("GooglePlay");
+            builder.Services.AddScoped<GooglePlayQueueProcessor>();
 
-			builder.Services.AddStoreConnector<GooglePlayConnector>("GooglePlay");
-			builder.Services.AddScoped<GooglePlayQueueProcessor>();
+            return builder;
+        }
+    }
 
-			return builder;
-		}
-	}
-
-	class SnakeCasePropertyNamingPolicy : JsonNamingPolicy
-	{
-		public override string ConvertName(string name) => name.ToSnakeCase();
-	}
+    class SnakeCasePropertyNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name) => name.ToSnakeCase();
+    }
 }
